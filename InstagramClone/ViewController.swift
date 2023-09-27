55ec79c79f621e7251617b73d0f6d8e6a35aec8d
BekirGeris <48486272+BekirGeris@users.noreply.github.com>
2023-09-28 02:25:50 +0300

    add login add home add update

--- InstagramClone/ViewController.swift
+++ InstagramClone/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signInClick(_ sender: Any) {
+        performSegue(withIdentifier: "toHome", sender: nil)
+    }
+    @IBAction func signUpClik(_ sender: Any) {
+        performSegue(withIdentifier: "toHome", sender: nil)
+    }
 }
 
